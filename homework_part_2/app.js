d14380fd2b39308dce17c6f6ac11864c61776316
Shane Guittap <shaneguittap@Kristins-MacBook-Pro-2.local>
2018-09-11 21:02:14 -0600

    9-11-homework complete

---
+++ homework_part_2/app.js
@@ -0,0 +1,95 @@
+/* concatenation: allows you to combine two strings together. 
+const name = 'shane';
+console.log('hey ' + name);
+*/
+
+/*interpolation: allows you to use a variable as a place holder and put them into strings. 
+const name = "shane";
+console.log(`hey ${name}!`);
+*/
+
+//dry: dont repeat yourself. so we dont repeat ourselves and clutter our code. functions, for/while loops. 
+
+/*declaring: defining the value of a variable. Assigning: allows you to set/re-set a value to a variable.
+define: figuring out what data type the variable is.
+*/
+
+//const: use when the variable is never going to change
+//let: when youre going to reassign the value of that variable
+
+//parent: when the folder has child directories or files in it. 
+
+//'man' to display the manual for any given command
+
+//its an autocomplete for what youre typing. it allows quick data entry
+
+/*const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kevin';
+
+console.log(a < b);
+console.log(c > d);
+console.log('Name' == 'Name');
+console.log(a < b < c);
+console.log(a <= a < d);
+console.log(e == 'Kevin');
+console.log(48 == '48');
+*/
+
+/*let letters = "A";
+//assinging a variable
+let i = 0;
+//assigning a variable
+while (i < 20) {
+    //while loop: checks to see if 'i' is less than 20
+    letters += "A";
+    //adds 'letters' to 'A' 
+    i++;
+    //adds one to 'i'
+}
+
+console.log(letters);
+//calls the function 'letters'
+//outcome: adds 'A' 20 times?
+*/
+
+/*let letters = "A";
+let i = 0;
+
+while (i < 20) {
+	letters += "A";
+	i++;
+}
+
+console.log(letters);
+*/
+
+//for loops: when you know how many times youre going to excecute the loop
+//while: when you dont know how many times you want it to keep going until condition returns false.
+//both allow for looping through as many times as necessary
+
+ 
+/*for (let i = 0; i <= 999; i++){
+    console.log('The number is ' + i);
+    }
+*/
+
+/*for (let i = 0; i < 100; i++) {
+	console.log('Without you, today\'s emotions are the scurf of yesterday\'s');
+}
+//(i=0) sets the position at the first index. initial expresion
+//(i<100) the function runs until 'i' is less than 100. condition
+//(i++) adds 1 to 'i'. increment expression
+*/   
+
+/* for (let i = 999; i >= 0; i--){
+    console.log('The number is ' + i);
+    }
+*/
+
+/* for (let i = 0; i <= 10; i++){
+    console.log(`The value of i is: ${i} of 10`);
+}
+*/
